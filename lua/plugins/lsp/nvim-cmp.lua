e3136846fe5441502aa6c4035d545f54a6271223
Anhar <anharbisri@gmail.com>
2024-04-27 18:33:29 +0700

    refine lsp

--- lua/plugins/lsp/nvim-cmp.lua
+++ lua/plugins/lsp/nvim-cmp.lua
@@ -1,45 +1,32 @@
-return {
-    'hrsh7th/nvim-cmp',
-    event = 'InsertEnter',
-    dependencies = {
-      {'L3MON4D3/LuaSnip'},
-    },
-    config = function()
-      -- Here is where you configure the autocompletion settings.
-      local lsp_zero = require('lsp-zero')
-      lsp_zero.extend_cmp()
+return   {
+  'hrsh7th/nvim-cmp',
+  event = 'InsertEnter',
+  dependencies = {
+    {'L3MON4D3/LuaSnip'},
+  },
+  config = function()
+    -- Here is where you configure the autocompletion settings.
+    local lsp_zero = require('lsp-zero')
+    lsp_zero.extend_cmp()
 
-      -- And you can configure cmp even more, if you want to.
-      local cmp = require('cmp')
-      local cmp_action = lsp_zero.cmp_action()
+    -- And you can configure cmp even more, if you want to.
+    local cmp = require('cmp')
+    local cmp_action = lsp_zero.cmp_action()
 
-      cmp.setup({
-        formatting = lsp_zero.cmp_format({details = true}),
-        mapping = {
-        ['<C-Enter>'] = cmp.mapping.confirm({select = false}),
-        ['<C-e>'] = cmp.mapping.abort(),
-        ['<Up>'] = cmp.mapping.select_prev_item({behavior = 'select'}),
-        ['<Down>'] = cmp.mapping.select_next_item({behavior = 'select'}),
-        ['<C-p>'] = cmp.mapping(function()
-          if cmp.visible() then
-            cmp.select_prev_item({behavior = 'insert'})
-          else
-            cmp.complete()
-          end
-        end),
-        ['<C-n>'] = cmp.mapping(function()
-          if cmp.visible() then
-            cmp.select_next_item({behavior = 'insert'})
-          else
-            cmp.complete()
-          end
-        end),
-        },
-        snippet = {
-          expand = function(args)
-            require('luasnip').lsp_expand(args.body)
-          end,
-        },
-      })
-    end
+    cmp.setup({
+      formatting = lsp_zero.cmp_format({details = true}),
+      mapping = cmp.mapping.preset.insert({
+        ['<C-Space>'] = cmp.mapping.complete(),
+        ['<C-u>'] = cmp.mapping.scroll_docs(-4),
+        ['<C-d>'] = cmp.mapping.scroll_docs(4),
+        ['<C-f>'] = cmp_action.luasnip_jump_forward(),
+        ['<C-b>'] = cmp_action.luasnip_jump_backward(),
+      }),
+      snippet = {
+        expand = function(args)
+          require('luasnip').lsp_expand(args.body)
+        end,
+      },
+    })
+  end
 }
