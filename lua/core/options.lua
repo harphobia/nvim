c3fd12e9e431a2461ae905808a5ff82c878f9767
Anhar Bisri <anharbisri@gmail.com>
2025-05-01 18:27:35 +0700

    fix after removing tree viewer

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -10,10 +10,6 @@ vim.opt.softtabstop = 2
 -- use number of spaces to insert a <Tab>
 vim.opt.expandtab = true
 
--- disable netrw at the very start of your init.lua
-vim.g.loaded_netrw = 1
-vim.g.loaded_netrwPlugin = 1
-
 -- optionally enable 24-bit colour
 vim.opt.termguicolors = true
 
